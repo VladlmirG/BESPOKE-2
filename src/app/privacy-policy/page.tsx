0ac9b6b5c701fd6072f240e929aeb766f1906cd8
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-09-17 01:19:50 +0200

    Update page.tsx

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -26,18 +26,18 @@ const PrivacyPolicy = () => {
         <div>
           <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
           <p>
-            Bespoke XV & Formal runs <strong><a href="https://bespokequince.com/" className="text-hovr">bespokequince.com</a></strong>. This portal aims to inform you of our policies regarding the collection, use, and disclosure of Personal Information we receive from users of the Site. We use your Personal Information only for providing and improving the Site. By using our third-party Booking and Events component on our Site, you expressly consent to our use and disclosure of your Personal Information in the manner described in this policy. Please check back frequently to ensure that you are familiar with our current policies and practices.
+         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
           <p>
-            While using our Site, we may ask you to provide certain personally identifiable information that can be used to contact or identify you. We do not collect Personal Information from children under 13 without parental consent. Personal Information may include, but is not limited to, your name, email address, phone number, or other data to help you with your user experience ("Personal Information"). This information is collected from you when you book a meeting on our site or enter information on our site. You can also contact us directly using the contact information provided on the site.
+   Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
-          <h4 className="text-xl font-bold mb-4">We may use the information we collect from you in the following ways:</h4>
+          <h4 className="text-xl font-bold mb-4">We may use the information we collect from you in the following ways</h4>
           <ul className="list-disc list-inside">
             <li>To personalize your experience.</li><br />
             <li>To enhance existing features or develop new features on the site.</li><br />
@@ -46,35 +46,35 @@ const PrivacyPolicy = () => {
           </ul>
 
           <p>
-            We may retain Personal Information provided to us indefinitely. In addition, we may share Personal Information with third parties to the extent reasonably necessary to: protect the security and integrity of our Website; to take precautions against liability; to respond to judicial process; or to the extent permitted under provisions of law, to provide information to law enforcement agencies or for an investigation related to public safety.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
           <h4 className="text-xl font-bold mb-4">Cookies Policy</h4>
           <p>
-            Cookies are files with a small amount of data, which may include an anonymous unique identifier, sent to your browser from a website and stored on your computer's hard drive for record-keeping purposes. Cookies are a common industry standard, and like many sites, we use cookies to collect information. Using cookies helps us remember users' visits, understand and save user preferences for future visits, keep track of advertisements, compile aggregate data about site traffic, and site interactions to offer better site experiences and tools in the future. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use our site to its full extent.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
           <h4 className="text-xl font-bold mb-4">Links To Other Websites</h4>
           <p>
-            Our Site may contain links to other websites, including those of sponsors and advertisers, whose information practices may be different from ours. Visitors should consult those other third-party websites' privacy notices, as they are not covered by our privacy policy and may follow different procedures.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
           <h4 className="text-xl font-bold mb-4">Security</h4>
           <p>
-            Securing your Personal Information is important to us, but keep in mind that no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
 
           <h4 className="text-xl font-bold mb-4">Changes To This Privacy Policy</h4>
           <p>
-            We may modify this Privacy Policy from time to time. We reserve the right to update or change our Privacy Policy at any time, and you should check this Privacy Policy periodically. Your continued use of our services after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide by and be bound by the modified Privacy Policy. If we make any changes to this Privacy Policy, we will notify you either through the email address you may have provided us, or by placing a prominent notice on our website.
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
           </p>
 
           <br /><br />
