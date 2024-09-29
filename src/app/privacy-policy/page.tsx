ff0a45f980c8168ccca940410baa1eaae56d5ecf
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-09-29 04:49:50 +0200

    Update page.tsx

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'; 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import React from 'react';
 
-export const metadata: Metadata = {
-  title: "BESPOKE XV & FORMAL | Privacy Policy",
-  description: "Bespoke XV & Formal Privacy Policy",
-};
+// export const metadata: Metadata = {
+//   title: "BESPOKE XV & FORMAL | Privacy Policy",
+//   description: "Bespoke XV & Formal Privacy Policy",
+// };
 
 const PrivacyPolicy = () => {
   return (
